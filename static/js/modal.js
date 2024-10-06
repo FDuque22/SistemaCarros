414df588e2ddded13b0a407ef9e7602c577629d8
Felipe Duque <lipeduque22@gmail.com>
2024-10-06 14:27:43 -0300

    TESTE HTML

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,11 +1,22 @@
+// Função para abrir o modal com a imagem
 function openModal(imageUrl) {
-    const modal = document.getElementById("imageModal");
-    const modalImage = document.getElementById("modalImage");
-    modal.style.display = "flex"; // Muda para "flex" para centralizar o conteúdo
+    console.log("Modal aberto com a imagem:", imageUrl);
+    var modal = document.getElementById("imageModal");
+    var modalImage = document.getElementById("modalImage");
+    modal.style.display = "flex"; // Alterar para "flex" para centralizar o conteúdo
     modalImage.src = imageUrl;
 }
 
+// Função para fechar o modal
 function closeModal() {
-    const modal = document.getElementById("imageModal");
+    var modal = document.getElementById("imageModal");
     modal.style.display = "none";
+}
+
+// Fechar o modal ao clicar fora da imagem
+window.onclick = function(event) {
+    var modal = document.getElementById("imageModal");
+    if (event.target === modal) { // Use "===" para comparação estrita
+        closeModal();
+    }
 }
