56cb171628f72e55866d9a549636c6d779a582b0
Felipe Duque <lipeduque22@gmail.com>
2024-10-16 13:05:06 -0300

    Perfil add

--- accounts/views.py
+++ accounts/views.py
@@ -31,6 +31,20 @@ def login_view(request):
         login_form = AuthenticationForm()
     return render(request, 'login.html', {'login_form': login_form})
 
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('cars_list')  # Redirecionar após login
+        else:
+            messages.error(request, "Usuário ou senha inválidos!")
+            login_form = AuthenticationForm()
+    return render(request, 'login.html', {'login_form': login_form})
+
+
 def logout_view(request):
     logout(request)
     messages.success(request, "Você foi desconectado com sucesso!")  # Mensagem de sucesso
